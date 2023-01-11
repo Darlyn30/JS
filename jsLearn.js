6832127d026e69c17c8f123d1389f6e6b13906cb
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-01-11 13:51:00 -0400

    Update jsLearn.js

--- jsLearn.js
+++ jsLearn.js
@@ -68,3 +68,85 @@ if (nota1>70 || nota1<80){
 
         }
 };
+
+
+
+
+
+//condicional dentro de una funcion
+
+function promedio (score1, score2 ){
+
+    var results = score1, score2;
+    if(score1>score2){
+        return results
+    } else{
+        console.log('ERROR')
+    }
+};
+
+var recibelo = promedio (100, 15);
+console.log(recibelo);
+
+//bucles
+
+/*como su nombre lo indica los bucles son un tipo de funcion que cumple con una condicion y se repite indefinidamente hasta que cumpla con la condicion asignada
+hay 2 formas de llamar a los bucles
+while
+for*/
+
+//                     EX                //
+
+// while
+
+let contador = 0;
+while(contador <5) {
+    console.log(contador)
+    contador = contador + 1
+};
+
+//en la consola te saldra como un camino desde el valor de la variable hasta lo que hara, en este caso menor que 5, osea que en 4 se detendra
+
+// for 
+
+for (let conter = 0; conter <10; conter = conter + 1){
+    console.log(conter)
+};
+
+// obtendra el mismo resultado que usando while, pero en menos lineas,
+
+
+// con el for se suelen llamar mas con la (i)
+for (let i = 0; i<5; i++) {
+    //el i++ significa lo mismo que i + 1 o variable+1
+    console.log(i)
+};
+
+
+// funciones
+
+/* las funciones son lo que le da la funcionalidad a nuestro projecto */
+
+//                   EX                   //
+
+function saludar(){
+    console.log('klk')
+};
+
+saludar ();
+
+function multiplicar(num1, num2){
+    let resultado = num1*num2
+    return resultado
+};
+
+// cuando hay un return se necesita una variable que almacene la funcion
+let recibidor = multiplicar(2, 5);
+console.log(recibidor)
+
+
+
+
+
+
+// TODO ESTA MOSTRADO SOLAMENTE POR CONSOLA TODO ESTE JS EN EL HTML
