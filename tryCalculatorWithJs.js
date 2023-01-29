7d0112944b50901b15727730878e7c06b22a436e
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-01-29 02:13:28 -0400

    Create tryCalculatorWithJs.js

---
+++ tryCalculatorWithJs.js
@@ -0,0 +1,288 @@
+console.log("hola")
+
+let dato1 = document.querySelector('#numero1');
+let dato2 = document.querySelector('#numero2');
+
+let op = document.querySelector('#operadores');
+
+let operar = document.querySelector('#operar');
+let resultado = document.querySelector('#result');
+
+
+function operar(){
+    if (op == '+'){
+        operar = dato1+dato2
+         
+    } else{
+        if(op == '-'){
+            operar = dato1-dato2
+             
+        } else{
+            if(op == '*'){
+                operar = dato1*dato2
+                 
+            } else{console.log("hola")
+
+let dato1 = document.querySelector('#numero1');
+let dato2 = document.querySelector('#numero2');
+
+let op = document.querySelector('#operadores');
+
+let operar = document.querySelector('#operar');
+let resultado = document.querySelector('#result');
+
+
+function operar(){
+    if (op == '+'){
+        operar = dato1+dato2
+         
+    } else{
+        if(op == '-'){
+            operar = dato1-dato2
+             
+        } else{
+            if(op == '*'){
+                operar = dato1*dato2
+                 
+            } else{
+                if(op == '/'){
+                    operar = dato1/dato2
+                     
+                }
+            }
+        }
+    }
+} 
+
+operar();
+
+function comparar(){
+    if(dato1<dato2){
+        window.alert('el dato1 es menor que el dato2')
+    } else{console.log("hola")
+
+let dato1 = document.querySelector('#numero1');
+let dato2 = document.querySelector('#numero2');
+
+let op = document.querySelector('#operadores');
+
+let operar = document.querySelector('#operar');
+let resultado = document.querySelector('#result');
+
+
+function operar(){
+    if (op == '+'){
+        operar = dato1+dato2
+         
+    } else{
+        if(op == '-'){
+            operar = dato1-dato2
+             
+        } else{
+            if(op == '*'){
+                operar = dato1*dato2
+                 
+            } else{
+                if(op == '/'){
+                    operar = dato1/dato2
+                     
+                }
+            }
+        }
+    }
+} 
+
+operar();
+
+function comparar(){
+    if(dato1<dato2){
+        window.alert('el dato1 es menor que el dato2')
+    } else{
+        if(dato1>dato2){
+            window.alert('el dato 1 es mayor que el dato 2')
+        } else{
+            if(dato1 == dato2){
+                window.alert('ambos datos son iguales')
+            }
+        }
+    }
+}
+
+//comparar();
+
+function bucle(){
+    if(dato1 < dato2){
+        for(i=0; i<5; i++){
+            console.log('hola que tal')
+        }
+    } else{
+        if(dato1>dato2){
+            for(i=0; i<10; i++){
+                console.log('el mejor')
+            }
+        } else{
+            if(dato1==dato2 ){
+                for(i=0; i>11; i++){
+                    console.log("i'm better than u")
+                }
+            }
+        }
+    }
+}
+
+function result(){
+    if (op == '+'){
+        resultado = dato1+dato2
+        onclick = result
+    
+    } else{
+        if(op == '-'){
+            resultado = dato1-dato2
+            onclick = result
+        } else{
+            if(op == '*'){
+                resultado = dato1*dato2
+                onclick = result
+            } else{
+                if(op == '/'){
+                    resultado = dato1/dato2
+                    onclick = result
+                }
+            }
+        }
+    }
+
+}
+
+result();
+
+        if(dato1>dato2){
+            window.alert('el dato 1 es mayor que el dato 2')
+        } else{
+            if(dato1 == dato2){
+                window.alert('ambos datos son iguales')
+            }
+        }
+    }
+}
+
+//comparar();
+
+function bucle(){
+    if(dato1 < dato2){
+        for(i=0; i<5; i++){
+            console.log('hola que tal')
+        }
+    } else{
+        if(dato1>dato2){
+            for(i=0; i<10; i++){
+                console.log('el mejor')
+            }
+        } else{
+            if(dato1==dato2 ){
+                for(i=0; i>11; i++){
+                    console.log("i'm better than u")
+                }
+            }
+        }
+    }
+}
+
+function result(){
+    if (op == '+'){
+        resultado = dato1+dato2
+        onclick = result
+    
+    } else{
+        if(op == '-'){
+            resultado = dato1-dato2
+            onclick = result
+        } else{
+            if(op == '*'){
+                resultado = dato1*dato2
+                onclick = result
+            } else{
+                if(op == '/'){
+                    resultado = dato1/dato2
+                    onclick = result
+                }
+            }
+        }
+    }
+
+}
+
+result();
+
+                if(op == '/'){
+                    operar = dato1/dato2
+                     
+                }
+            }
+        }
+    }
+} 
+
+operar();
+
+function comparar(){
+    if(dato1<dato2){
+        window.alert('el dato1 es menor que el dato2')
+    } else{
+        if(dato1>dato2){
+            window.alert('el dato 1 es mayor que el dato 2')
+        } else{
+            if(dato1 == dato2){
+                window.alert('ambos datos son iguales')
+            }
+        }
+    }
+}
+
+//comparar();
+
+function bucle(){
+    if(dato1 < dato2){
+        for(i=0; i<5; i++){
+            console.log('hola que tal')
+        }
+    } else{
+        if(dato1>dato2){
+            for(i=0; i<10; i++){
+                console.log('el mejor')
+            }
+        } else{
+            if(dato1==dato2 ){
+                for(i=0; i>11; i++){
+                    console.log("i'm better than u")
+                }
+            }
+        }
+    }
+}
+
+function result(){
+    if (op == '+'){
+        resultado = dato1+dato2
+        onclick = result
+    
+    } else{
+        if(op == '-'){
+            resultado = dato1-dato2
+            onclick = result
+        } else{
+            if(op == '*'){
+                resultado = dato1*dato2
+                onclick = result
+            } else{
+                if(op == '/'){
+                    resultado = dato1/dato2
+                    onclick = result
+                }
+            }
+        }
+    }
+
+}
+
+result();
