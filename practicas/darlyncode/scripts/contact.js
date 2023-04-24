52ae3276661a16ecc3bc059740c08e0360ee3189
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-04-24 13:47:15 -0800

    Delete practicas/darlyncode directory

--- practicas/darlyncode/scripts/contact.js
+++
@@ -1,9 +0,0 @@
-const volver = document.getElementById("atras")
-
-volver.addEventListener("click", volverPrincipal)
-
-
-function volverPrincipal(){
-    console.log("hola mundo")
-    window.location = "/index.html"
-}
